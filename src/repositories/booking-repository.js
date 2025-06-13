f056e76b7b62f1f3bc7ffd77ea00a106add55972
Gaurav singh lodhi <lodhigaurav23@gmail.com>
2025-06-13 19:00:20 +0530

    Added MVC  boilerplate for booking

---
+++ src/repositories/booking-repository.js
@@ -0,0 +1,13 @@
+const { StatusCodes } = require("http-status-codes");
+
+const {Booking}  = require('../models');
+
+const CrudRepository =  require('./crud-repositories')
+
+class BookingRepository extends CrudRepository{
+  constructor() {
+    super(Booking)
+  }
+}
+
+module.exports = BookingRepository;
